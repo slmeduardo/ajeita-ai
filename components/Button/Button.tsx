c9be08712effd27df9122fd7fc7ecf6364666a5d
slmeduardo <itsmeeduardoc@gmail.com>
2022-12-19 16:04:20 -0300

    fix: type button

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,4 +1,4 @@
-export default function Button({ children }) {
+export default function Button({ children }: { children: string }) {
   return (
     <button className="bg-secondary shadow-xl shadow-green-300 text-white rounded px-5 py-2">
       {children}
